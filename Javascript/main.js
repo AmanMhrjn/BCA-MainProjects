faf17597aced87689fee369d8b5693e86582a872
AmanMhrjn <aman9861889793maharjan@gmail.com>
2022-02-21 14:58:22 +0545

    Updated JS Validation

---
+++ Javascript/main.js
@@ -0,0 +1,76 @@
+function validateDetails(){
+    userName = document.getElementById('username').value;
+    email = document.getElementById('email').value;
+    phone = document.getElementById('phone').value;
+    numberOfGuests = document.getElementById('numberOfGuests').value;
+
+
+    userNameError = document.getElementById('username-error');
+    emailError = document.getElementById('email-error');
+    phoneError = document.getElementById('phone-error');
+    numberOfGuestError = document.getElementById('numberofguest-error');
+
+    if(userName == ""){
+        userNameError.innerHTML="Username cannot be EMPTY";
+        userNameError.style.display="block";
+    }else{
+        if(userName.length<8){
+            userNameError.innerHTML="Username has less then 8 characters";
+            userNameError.style.display="block";
+        }
+    }
+
+    if(email == ""){
+        emailError.innerHTML="Email cannot be EMPTY";
+        emailError.style.display="block";
+    } 
+
+    if(phone == ""){
+        phoneError.innerHTML="Phone number cannot be EMPTY";
+        phoneError.style.display="block";
+    }else{
+        if(phone.length<=10){
+            phoneError.innerHTML="Phone number cannot be less than 10 digits";
+            phoneError.style.display="block";
+        }else{
+            if(phone.length>10){
+                phoneError.innerHTML="Phone number cannot be greater than 10 digits";
+                phoneError.style.display="block";
+            }
+        }
+    }
+
+    if(numberOfGuests == ""){
+        numberOfGuestError.innerHTML="Number of guest is EMPTY";
+        numberOfGuestError.style.display="block";
+    }
+
+}
+
+function validateContact(){
+    firstName = document.getElementById('fname').value;
+    lastName = document.getElementById('lname').value;
+    email = document.getElementById('email').value;
+
+    fNameError = document.getElementById('fname-error');
+    lNameError = document.getElementById('lname-error');
+    emailError = document.getElementById('email-error');
+
+
+    if(firstName == ""){
+        fNameError.innerHTML="First name cannot be empty";
+        fNameError.style.display="block";
+    }
+
+    if(lastName == ""){
+        lNameError.innerHTML="Last name cannot be empty";
+        lNameError.style.display="block";
+    }
+
+    if(email == ""){
+        emailError.innerHTML="Email cannot be empty";
+        emailError.style.display="block";
+    }
+
+
+}
